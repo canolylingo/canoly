f1e751fd7d6dc54810ffb204296ac37a01bf6a70
canolylingo <umitcananmorgul@gmail.com>
2026-03-23 10:47:27 +0300

    Delete components directory

--- components/BubbleCard.tsx
+++
@@ -1,9 +0,0 @@
-export function BubbleCard({
-  children,
-  className = "",
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) {
-  return <div className={`bubble p-5 ${className}`}>{children}</div>;
-}
